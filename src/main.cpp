00b4a115501b40a0c72e4f9ec321156f82573788
SommeanDev <shivanshmourya03@gmail.com>
2024-04-11 20:53:00 +0530

    Changed file extension from .tl to .tal

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 int main (int argc, char *argv[]) {
   if (argc < 2) {
     std::cerr << "Incorrect usage. Use instead: " << std::endl;
-    std::cerr << "tlan <file.tl>\n";
+    std::cerr << "tlan <file.tal>\n";
     return EXIT_FAILURE;
   }
   
